6485a5ec77d05c6686415a177429e788c6e08a66
hayabusa <s.hashimoto@hs-sh.net>
2023-02-25 19:10:18 +0900

    add prefab

---
+++ Editor/CreateUdonXML.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace UdonXMLParser
+{
+    public class CreateUdonXML
+    {
+        [MenuItem("GameObject/nekomimiStudio/UdonXML", false, 10)]
+        public static void Create(MenuCommand menu)
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/studio.nekomimi.udonxml/Runtime/UdonXML.prefab");
+            GameObject res = (GameObject)UnityEditor.PrefabUtility.InstantiatePrefab(prefab);
+            GameObjectUtility.SetParentAndAlign(res, (GameObject)menu.context);
+            Undo.RegisterCreatedObjectUndo(res, "UdonXML");
+            Selection.activeObject = res;
+        }
+    }
+}
